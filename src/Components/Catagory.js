2d68af7890c3a5b83e07d4667f290235ad11cdfc
Soumya Sachin Panda <pandasoumyasachin@gmail.com>
2023-11-14 00:23:56 +0530

    added useNavigate hook for search functionality.

--- src/Components/Catagory.js
+++ src/Components/Catagory.js
@@ -2,15 +2,33 @@ import React, { useEffect, useState } from "react";
 import { CATEGORY_VIDEOS_API } from "../Utils/Constant";
 import { Link, useParams } from "react-router-dom";
 import VideoCart from "./VideoCart";
+import { useSelector } from "react-redux";
+
+import { useNavigate } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { searchResullts } from "../Utils/SearchValue";
 
 const Catagory = () => {
   const [catVideos, setCatVideos] = useState("");
+  // const [check , setCheck] = useState(false);
+
+  const searchResult = useSelector((store) => store.searchValue.results);
+  const dispatch = useDispatch();
   const params = useParams();
+  const navigate = useNavigate();
   console.log(params);
   const { id } = params;
 
   useEffect(() => {
     getData(id);
+
+    console.log("Hello");
+    if (searchResult) {
+     
+      dispatch(searchResullts(""));
+    }
+     // eslint-disable-next-line
   }, [id]);
 
   const getData = async (id) => {
@@ -22,11 +40,26 @@ const Catagory = () => {
     setCatVideos(json.items);
   };
 
+  const goNavigate = () => {
+    // eslint-disable-next-line
+    navigate("/");
+  };
+
+  if (searchResult) {
+    goNavigate();
+  } else {
+    console.log("hii");
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 w-full">
       {catVideos &&
         catVideos.map((item) => {
-          return <Link to={`/watch?v=${item.id}`}><VideoCart info={item} /></Link>;
+          return (
+            <Link to={`/watch?v=${item.id}`}>
+              <VideoCart info={item} />
+            </Link>
+          );
         })}
     </div>
   );
